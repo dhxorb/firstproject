f10f020ed115e553af29d0dfce21d6de74797763
dhxorb <osaeroi@naver.com>
2023-07-12 17:22:27 +0900

    sixth commit

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,37 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", toDos);
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li")
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit() {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value ="";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+//append는 맨뒤에 넣는다.
